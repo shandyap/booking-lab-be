91b6d13920e3103677a274add0692870f3400df3
Shandy Ariady <shandylatoz851@gmail.com>
2025-08-01 09:51:00 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
